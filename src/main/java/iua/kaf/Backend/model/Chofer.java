374ad0b407128c0b3624ba97b1416d439c839c4a
Faq <69550808+Faq-hue@users.noreply.github.com>
2022-10-07 16:00:13 -0300

    hechos los modelos camion y chofer, pero faltan sus uniones

--- src/main/java/iua/kaf/Backend/model/Chofer.java
+++ src/main/java/iua/kaf/Backend/model/Chofer.java
@@ -1,12 +1,35 @@
 package iua.kaf.Backend.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "choferes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Chofer {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(unique = false, nullable = false)
 	private String nombre;
 	
+	@Column(unique = false, nullable = false)
 	private String apellido;
 	
+	@Column(unique = true, nullable = false)
 	private long dni;
 }
