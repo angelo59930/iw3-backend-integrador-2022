04fecdf704641017744b3fb61eb3884cd79d87ce
angelo59930 <manfrediangelosantiago10@gmail.com>
2022-10-12 14:54:25 -0300

    cambio de nombre en el paquete

---
+++ src/main/java/iua/kaf/Backend/model/business/exception/BusinessException.java
@@ -0,0 +1,25 @@
+package iua.kaf.Backend.model.business.exception;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class BusinessException extends Exception {
+
+	private static final long serialVersionUID = -8582277206660722157L;
+
+	@Builder
+	public BusinessException(String message, Throwable ex) {
+		super(message, ex);
+	}
+	@Builder
+	public BusinessException(String message) {
+		super(message);
+	}
+	@Builder
+	public BusinessException(Throwable ex) {
+		super(ex.getMessage(), ex);
+	}
+
+
+}
---
+++ src/main/java/iua/kaf/Backend/model/business/exception/FoundException.java
@@ -0,0 +1,23 @@
+package iua.kaf.Backend.model.business.exception;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class FoundException extends Exception {
+
+	private static final long serialVersionUID = -8582277206660722157L;
+
+	@Builder
+	public FoundException(String message, Throwable ex) {
+		super(message, ex);
+	}
+	@Builder
+	public FoundException(String message) {
+		super(message);
+	}
+	@Builder
+	public FoundException(Throwable ex) {
+		super(ex.getMessage(), ex);
+	}
+}
