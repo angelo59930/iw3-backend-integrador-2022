7c647d837d473a3692bf17a851c7c169d8336960
kyanesdev <kyanesdev@gmail.com>
2022-10-16 17:48:51 -0300

    Agregue constante de URL_PRODUCTOS

--- src/main/java/iua/kaf/Backend/controller/Constantes.java
+++ src/main/java/iua/kaf/Backend/controller/Constantes.java
@@ -6,6 +6,7 @@ public class Constantes {
     public static final String URL_API_VERSION = "/v1";
     public static final String URL_BASE = URL_API + URL_API_VERSION;
     public static final String URL_ORDEN = URL_BASE + "/orden";
+    public static final String URL_PRODUCTOS = URL_BASE + "/productos";
 
     public static final String URL_INTEGRATION = URL_BASE + "/integration";
     public static final String URL_INTEGRATION_CLI1 = URL_INTEGRATION + "/cli1";
--- src/main/java/iua/kaf/Backend/controller/ProductoRestController.java
+++ src/main/java/iua/kaf/Backend/controller/ProductoRestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping(Constantes.URL_PRODUCTOS)
 public class ProductoRestController {
   
 }
