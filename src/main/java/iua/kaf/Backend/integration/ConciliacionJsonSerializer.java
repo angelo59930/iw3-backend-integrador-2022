41b238480a971b2ca3a9096351031e55116da16b
faq <fzamora994@alumnos.iua.edu.ar>
2022-10-29 12:56:11 -0300

    creado el serializador de conciliacion

---
+++ src/main/java/iua/kaf/Backend/integration/ConciliacionJsonSerializer.java
@@ -0,0 +1,34 @@
+package iua.kaf.Backend.integration;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import iua.kaf.Backend.model.Conciliacion;
+
+public class ConciliacionJsonSerializer extends StdSerializer<Conciliacion>{
+
+	private static final long serialVersionUID = -6786295934606599185L;
+
+	protected ConciliacionJsonSerializer(Class<?> t, boolean dummy) {
+		super(t, dummy);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void serialize(Conciliacion value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+		gen.writeStartObject();
+		gen.writeNumberField("pesajeInicial", value.getPesajeInicial());
+		gen.writeNumberField("pesajeFinal", value.getPesajeFinal());
+		gen.writeNumberField("productoCargado", value.getProductoCargado());
+		gen.writeNumberField("netoPorBalanza", value.getNetoPorBalanza());
+		gen.writeNumberField("diferenciaBalanzaCaudalimetro", value.getDiferenciaBalanzaCaudalimetro());
+		gen.writeNumberField("promedioTemperatura", value.getPromedioTemperatura());
+		gen.writeNumberField("promedioDensidad", value.getPromedioDensidad());
+		gen.writeNumberField("promedioCaudal", value.getPromedioCaudal());
+		gen.writeEndObject();
+		
+	}
+
+}
