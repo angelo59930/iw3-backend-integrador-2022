2dba99d9231205b05b984d0622480e110225dd08
faq <fzamora994@alumnos.iua.edu.ar>
2022-11-01 15:58:07 -0300

    mod conciliacion

--- src/main/java/iua/kaf/Backend/controller/ConciliacionRestController.java
+++ src/main/java/iua/kaf/Backend/controller/ConciliacionRestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import iua.kaf.Backend.model.Conciliacion;
 import iua.kaf.Backend.model.business.IConciliacionBusiness;
 import iua.kaf.Backend.model.business.exception.BusinessException;
