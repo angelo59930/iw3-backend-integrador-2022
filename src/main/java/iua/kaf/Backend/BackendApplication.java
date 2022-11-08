1ea5099dc65007af16190bde96808200df019fad
faq <fzamora994@alumnos.iua.edu.ar>
2022-11-08 14:40:52 -0300

    hot fix

--- src/main/java/iua/kaf/Backend/BackendApplication.java
+++ src/main/java/iua/kaf/Backend/BackendApplication.java
@@ -1,11 +1,13 @@
 package iua.kaf.Backend;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -14,5 +16,8 @@ public class BackendApplication {
 
 	@Value("${spring.profiles.active}")
 	private String profile;
+
+
+	
 	
 }
