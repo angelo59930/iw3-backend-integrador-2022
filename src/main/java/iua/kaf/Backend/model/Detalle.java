c125dfb0d36d46ea11b9458abedfb5861d7d2861
faq <fzamora994@alumnos.iua.edu.ar>
2022-11-11 08:15:46 -0300

    cambiada la relacion de detalle a orden por ManyToOne

--- src/main/java/iua/kaf/Backend/model/Detalle.java
+++ src/main/java/iua/kaf/Backend/model/Detalle.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -44,7 +44,7 @@ public class Detalle {
   @Column(nullable = true)
   private int cantidadActualizaciones;
 
-  @OneToOne()
+  @ManyToOne()
 	@JoinColumn(name = "id_orden", nullable = false)
   private Orden orden;
 
