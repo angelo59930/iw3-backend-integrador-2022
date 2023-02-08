81410ac2f7b9993954e2d7c158a0803e706cd5b6
faq <fzamora994@alumnos.iua.edu.ar>
2023-02-07 22:03:55 -0300

    5 test unitariios medio pelo

--- src/test/java/iua/kaf/Backend/BackendApplicationTests.java
+++ src/test/java/iua/kaf/Backend/BackendApplicationTests.java
@@ -2,15 +2,19 @@ package iua.kaf.Backend;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+import iua.kaf.Backend.model.Alerta;
 import iua.kaf.Backend.model.Camion;
+import iua.kaf.Backend.model.Chofer;
+import iua.kaf.Backend.model.Mail;
+import iua.kaf.Backend.model.Producto;
+import iua.kaf.Backend.model.business.AlertaBusiness;
 import iua.kaf.Backend.model.business.CamionBusiness;
+import iua.kaf.Backend.model.business.ChoferBusiness;
+import iua.kaf.Backend.model.business.MailBusiness;
+import iua.kaf.Backend.model.business.ProductoBusiness;
 import iua.kaf.Backend.model.business.exception.BusinessException;
 import iua.kaf.Backend.model.business.exception.NotFoundException;
 
@@ -19,7 +23,19 @@ class BackendApplicationTests {
 
 	@Autowired
 	private CamionBusiness camionBusiness;
+	
+	@Autowired
+	private ChoferBusiness choferBusiness;
+	
+	@Autowired
+	private MailBusiness mailBusiness;
 
+	@Autowired
+	private AlertaBusiness alertaBusiness;
+	
+	@Autowired
+	private ProductoBusiness productoBusiness;
+	
 	@Test
 	void contextLoads() {
 
@@ -33,6 +49,41 @@ class BackendApplicationTests {
 			e.printStackTrace();
 		}
 	}
-
+	
+	@Test
+	void choferTypeTest() {
+		try {
+			Assertions.assertSame(new Chofer().getClass(), choferBusiness.load(1).getClass());
+		} catch (NotFoundException | BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void mailTypeTest() {
+		try {
+			Assertions.assertSame(new Mail().getClass(), mailBusiness.load(1).getClass());
+		} catch (NotFoundException | BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void alertaTypeTest() {
+		try {
+			Assertions.assertSame(new Alerta().getClass(), alertaBusiness.load(1).getClass());
+		} catch (NotFoundException | BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void productoTypeTest() {
+		try {
+			Assertions.assertSame(new Producto().getClass(), productoBusiness.load(1).getClass());
+		} catch (NotFoundException | BusinessException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
